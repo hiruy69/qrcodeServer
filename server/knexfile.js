5fc07aafeb204db3f6e48011491e7d26fd44daa2
Hiruy <91972582+hiruy69@users.noreply.github.com>
2021-12-21 20:07:40 +0300

    database

--- server/knexfile.js
+++ server/knexfile.js
@@ -26,9 +26,18 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './db.sqlite3'
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   }
 
